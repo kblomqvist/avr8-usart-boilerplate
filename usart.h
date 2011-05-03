cc12b2708d2b66f3fe9449d07e7dbf405fdc19f3
kblomqvist <kblomqvist@iki.fi>
2011-05-03 22:07:00 +0300

    retab

--- usart.h
+++ usart.h
@@ -4,10 +4,10 @@
 #define USART_DATARECEIVED (UCSR0A & (1 << RXC0))
 #define USART_ESCAPE (USART_DATARECEIVED && (getchar() == 27))
 
-/*	Send character */
+/*  Send character */
 int usart_putchar(char c, FILE *stream);
 
-/*	Receive character */
+/*  Receive character */
 uint8_t usart_getchar(FILE *stream);
 
 #endif
